ed40f3a7deeb355d5e9c03dcc55161a6b61d918a
Fajar Legianto A <99655019+FajarLegiantoA@users.noreply.github.com>
2022-02-25 19:29:46 +0700

    Create insertion_sort.c

---
+++ sorting/Insertion_Sort/insertion_sort.c
@@ -0,0 +1,37 @@
+// Insertion sort
+#include <stdio.h>
+
+void cetak_array(int array[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+void insertionSort(int array[], int size)
+{
+  for (int step = 1; step < size; step++)
+  {
+    int key = array[step];
+    int j = step - 1;
+    while (key < array[j] && j >= 0)
+    {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
+
+int main()
+{
+  int data[] = {2, 5, 25, 13, 97, 142, 33, 21};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  printf("Array diurutkan dari yang terkecil:\n");
+  cetak_array(data, size);
+}
+
+// code from https://www.programiz.com/dsa/insertion-sort
